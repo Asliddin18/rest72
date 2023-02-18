e9c6f904a79d24c0aca2ab5bf365f9c389362a82
simple123w <122342661+Asliddin18@users.noreply.github.com>
2023-02-18 18:35:16 +0500

    a
    a

--- App.js
+++ App.js
@@ -13,11 +13,11 @@ app.use(Upload())
 app.use(cors())
 
 /* login */
-/* app.post('/login', (req, res) => {
+app.post('/login', (req, res) => {
   const reqUsername = req.body.name
   const password = req.body.password
   const accessToken = jwt.sign(reqUsername, process.env.ACCESS_TOKEN_SECRET)
-  var admins = "SELECT * FROM admins"
+  var admins = "SELECT * FROM Admins"
   connection.query(admins, (err, result) => {
     var loginCheck = false
     result.map(item => {
@@ -28,14 +28,15 @@ app.use(cors())
     if (loginCheck) {
       res.status(200).send(accessToken)
     } else {
-      res.status(503).send("qo`chqorvoy Token Xatoooo")
+      res.status(503).send("Password or Name Error")
     }
   })
 }) 
- */
+
+
 /* admins */
-/* app.get("/admins", authenticateToken, (req, res) => {
-  var adminsGet = "SELECT * FROM admins"
+app.get("/admins", (req, res) => {
+  var adminsGet = "SELECT * FROM Admins"
   connection.query(adminsGet, (err, result) => {
     if (!err) {
       res.status(200).send(result)
@@ -53,7 +54,7 @@ app.post("/admins", (req, res) => {
   var nickname = req.body.nickname
   var Id = uuid.v4()
 
-  connection.query("INSERT INTO admins values(?, ?, ?, ?, ?, ?, ?, ?)", [Id, name, password, "admin", surname, date, passport, nickname], (err, result) => {
+  connection.query("INSERT INTO Admins values(?, ?, ?, ?, ?, ?, ?, ?)", [Id, name, password, "admin", surname, date, passport, nickname], (err, result) => {
     if (!err) {
       res.status(201).send("User Added Successfully")
     } else {
@@ -63,7 +64,7 @@ app.post("/admins", (req, res) => {
 })
 app.delete("/admins/:id", (req, res) => {
   const id = req.params.id
-  connection.query("DELETE FROM admins WHERE id=?", [id], (err, result) => {
+  connection.query("DELETE FROM Admins WHERE id=?", [id], (err, result) => {
 
     if (!err) {
       if (result.affectedRows == 0) {
@@ -80,7 +81,7 @@ app.put("/admins/:id", (req, res) => {
   const id = req.params.id
   const ReqBody = req.body
 
-  connection.query("UPDATE admins SET name=?, password=?, category=?, surname=?, date=?, passport=?, nickname=? WHERE id=?", [ReqBody.name, ReqBody.password, "admin", ReqBody.surname, ReqBody.date, ReqBody.passport, ReqBody.nickname, id], (err, result) => {
+  connection.query("UPDATE Admins SET name=?, password=?, category=?, surname=?, date=?, passport=?, nickname=? WHERE id=?", [ReqBody.name, ReqBody.password, "admin", ReqBody.surname, ReqBody.date, ReqBody.passport, ReqBody.nickname, id], (err, result) => {
     if (!err) {
       if (result.affectedRows == 0) {
         res.status(400).send('Admin is does not found')
@@ -91,12 +92,12 @@ app.put("/admins/:id", (req, res) => {
       res.status(500).send(err)
     }
   })
-}) */
+})
 
 
 /* users */
-/* app.get("/users", (req, res) => {
-  var userGet = "SELECT * FROM users"
+app.get("/users", (req, res) => {
+  var userGet = "SELECT * FROM Persons"
   connection.query(userGet, (err, result) => {
     if (!err) {
       res.status(200).send(result)
@@ -104,14 +105,14 @@ app.put("/admins/:id", (req, res) => {
       res.status(400).send("Users was not get")
     }
   })
-}) */
-/* app.post("/users", (req, res) => {
+})
+app.post("/users", (req, res) => {
   const Id = uuid.v4()
   const R = req.body
 
-  connection.query("INSERT INTO users values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
+  connection.query("INSERT INTO Persons values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
     [Id, R.UserName, R.Date, R.Jinsi, R.Nogironligi, R.RuxiyHolat, R.Ishjoyi, R.lavozimi, R.OilaAxvoli, R.VoyYetmaganFarzandi, R.MuqaddamSud, R.MuqaddamVaqti, R.AyblovQisqa, R.JinoyatVaqti, R.JinoyatHudud, R.JinoyatJoyi, R.UshlanganSana, R.AybElon, R.AybModda, R.AybModdaQism, R.AybModdaBandi, R.AybOrgani, R.AybElonFIO, R.QamoqBolmagan, R.QamoqExtiyot, R.QamoqUy, R.AyblovProkLavozim, R.AyblovProkFIO, R.JinoyatZarar, R.ZararQoplanish,
-      R.MolMulQoplanish, R.QoplanishFoiz, R.IshProk, R.IshPorkFIO, R.AybTopish, R.Moddasi, R.Qismi, R.Bandi, R.AyblovVozKech, R.JazoTur, R.JazoMiqdori, R.ShartliHukm, R.Modda57, R.Modda96, R.MuddatOtib, R.XavfYoqot, R.Pushaymon, R.Yarashgan, R.Kasallik, R.AktiTufay, R.userscol, R.IjYoq, R.AmaldaPushay, R.SSudHukmSana, R.SSudyaFIO, R.SAybliModda, R.SAybliQismi, R.SAybliBandi, R.SAydanVoz, R.SJazoTuri, R.SJazoMiq, R.SShartli, R.S57Modda, R.S96Modda, R.SOtibket, R.SIjXavf, R.SAmaldaPush, R.SYarshMun, R.SKasallik, R.SAktAs, R.SYoq, R.SAmal, R.Ijkattabol, R.Unchaogir, R.Ogir, R.OtaOgir, R.TumanXul, R.ShaharXul, R.BoshXul, R.Category],
+      R.MolMulQoplanish, R.QoplanishFoiz, R.IshProk, R.IshPorkFIO, R.AybTopish, R.Moddasi, R.Qismi, R.Bandi, R.AyblovVozKech, R.JazoTur, R.JazoMiqdori, R.ShartliHukm, R.Modda57, R.Modda96, R.MuddatOtib, R.XavfYoqot, R.Pushaymon, R.Yarashgan, R.Kasallik, R.AktiTufay, R.userscol, R.IjYoq, R.AmaldaPushay, R.SSudHukmSana, R.SSudyaFIO, R.SAybliModda, R.SAybliQismi, R.SAybliBandi, R.SAydanVoz, R.SJazoTuri, R.SJazoMiq, R.SShartli, R.S57Modda, R.S96Modda, R.SOtibket, R.SIjXavf, R.SAmaldaPush, R.SYarshMun, R.SKasallik, R.SAktAs, R.SYoq, R.SAmal, R.Ijkattabol, R.Unchaogir, R.Ogir, R.OtaOgir, R.TumanXul, R.ShaharXul, R.BoshXul],
     (err, result) => {
       if (!err) {
         res.status(201).send("User Added Successfully")
@@ -140,8 +141,8 @@ app.put("/users/:id", (req, res) => {
   const Id = req.params.id
   const R = req.body
 
-  connection.query("UPDATE users SET Username=?, Date=?, Jinsi=?, Nogironligi=?, ruxiyXolat=?, IshJoyi=?, Lavozimi=?, oilaviyAxvoli=?, voyFarzandi=?, muqaddamSud=?, MuqddamVaqti=?, AybloqQizqa=?, JinoyatVaqti=?, JinoyatHudud=?, JinoyatJoyi=?, UshlanganSana=?, AybElon=?, AybModda=?, AybModdaQism=?, AybModdaBandi=?, AybOrgani=?, AybElonFIO=?, QamoqBolmagan=?, QamoqExtiyot=?, QamoqUy=?, AyblovProkLavozim=?, AyblovProkFIO=?, JinoyatZarar=?, ZararQoplanish=?, MolMulQoplanish=?, QoplanishFoiz=?, IshProk=?, IshPorkFIO=?, AybTopish=?, Moddasi=?, Qismi=?, Bandi=?, AyblovVozKech=?, JazoTur=?, JazoMiqdori=?, ShartliHukm=?, Modda57=?, Modda96=?, MuddatOtib=?, XavfYoqot=?, Pushaymon=?, Yarashgan=?, Kasallik=?, AktiTufay=?, userscol=?, IjYoq=?, AmaldaPushay=?, SSudHukmSana=?, SSudyaFIO=?, SAybliModda=?, SAybliQismi=?, SAybliBandi=?, SAydanVoz=?, SJazoTuri=?, SJazoMiq=?, SShartli=?, S57Modda=?, S96Modda=?, SOtibket=?, SIjXavf=?, SAmaldaPush=?, SYarshMun=?, SKasallik=?, SAktAs=?, SYoq=?, SAmal=?, Ijkattabol=?, Unchaogir=?, Ogir=?, OtaOgir=?, TumanXul=?, ShaharXul=?, BoshXul=?, Category=? WHERE id=?", 
-  [R.UserName, R.Date, R.Jinsi, R.Nogironligi, R.RuxiyHolat, R.Ishjoyi, R.lavozimi, R.OilaAxvoli, R.VoyYetmaganFarzandi, R.MuqaddamSud, R.MuqaddamVaqti, R.AyblovQisqa, R.JinoyatVaqti, R.JinoyatHudud, R.JinoyatJoyi, R.UshlanganSana, R.AybElon, R.AybModda, R.AybModdaQism, R.AybModdaBandi, R.AybOrgani, R.AybElonFIO, R.QamoqBolmagan, R.QamoqExtiyot, R.QamoqUy, R.AyblovProkLavozim, R.AyblovProkFIO, R.JinoyatZarar, R.ZararQoplanish, R.MolMulQoplanish, R.QoplanishFoiz, R.IshProk, R.IshPorkFIO, R.AybTopish, R.Moddasi, R.Qismi, R.Bandi, R.AyblovVozKech, R.JazoTur, R.JazoMiqdori, R.ShartliHukm, R.Modda57, R.Modda96, R.MuddatOtib, R.XavfYoqot, R.Pushaymon, R.Yarashgan, R.Kasallik, R.AktiTufay, R.userscol, R.IjYoq, R.AmaldaPushay, R.SSudHukmSana, R.SSudyaFIO, R.SAybliModda,R.SAybliQismi, R.SAybliBandi, R.SAydanVoz, R.SJazoTuri, R.SJazoMiq, R.SShartli, R.S57Modda, R.S96Modda, R.SOtibket, R.SIjXavf, R.SAmaldaPush, R.SYarshMun, R.SKasallik, R.SAktAs, R.SYoq, R.SAmal, R.Ijkattabol, R.Unchaogir, R.Ogir, R.OtaOgir, R.TumanXul, R.ShaharXul, R.BoshXul, R.Category, Id], 
+  connection.query("UPDATE Persons SET Username=?, Date=?, Jinsi=?, Nogironligi=?, ruxiyXolat=?, IshJoyi=?, Lavozimi=?, oilaviyAxvoli=?, voyFarzandi=?, muqaddamSud=?, MuqddamVaqti=?, AybloqQizqa=?, JinoyatVaqti=?, JinoyatHudud=?, JinoyatJoyi=?, UshlanganSana=?, AybElon=?, AybModda=?, AybModdaQism=?, AybModdaBandi=?, AybOrgani=?, AybElonFIO=?, QamoqBolmagan=?, QamoqExtiyot=?, QamoqUy=?, AyblovProkLavozim=?, AyblovProkFIO=?, JinoyatZarar=?, ZararQoplanish=?, MolMulQoplanish=?, QoplanishFoiz=?, IshProk=?, IshPorkFIO=?, AybTopish=?, Moddasi=?, Qismi=?, Bandi=?, AyblovVozKech=?, JazoTur=?, JazoMiqdori=?, ShartliHukm=?, Modda57=?, Modda96=?, MuddatOtib=?, XavfYoqot=?, Pushaymon=?, Yarashgan=?, Kasallik=?, AktiTufay=?, userscol=?, IjYoq=?, AmaldaPushay=?, SSudHukmSana=?, SSudyaFIO=?, SAybliModda=?, SAybliQismi=?, SAybliBandi=?, SAydanVoz=?, SJazoTuri=?, SJazoMiq=?, SShartli=?, S57Modda=?, S96Modda=?, SOtibket=?, SIjXavf=?, SAmaldaPush=?, SYarshMun=?, SKasallik=?, SAktAs=?, SYoq=?, SAmal=?, Ijkattabol=?, Unchaogir=?, Ogir=?, OtaOgir=?, TumanXul=?, ShaharXul=?, BoshXul=? WHERE UserId`=?", 
+  [R.UserName, R.Date, R.Jinsi, R.Nogironligi, R.RuxiyHolat, R.Ishjoyi, R.lavozimi, R.OilaAxvoli, R.VoyYetmaganFarzandi, R.MuqaddamSud, R.MuqaddamVaqti, R.AyblovQisqa, R.JinoyatVaqti, R.JinoyatHudud, R.JinoyatJoyi, R.UshlanganSana, R.AybElon, R.AybModda, R.AybModdaQism, R.AybModdaBandi, R.AybOrgani, R.AybElonFIO, R.QamoqBolmagan, R.QamoqExtiyot, R.QamoqUy, R.AyblovProkLavozim, R.AyblovProkFIO, R.JinoyatZarar, R.ZararQoplanish, R.MolMulQoplanish, R.QoplanishFoiz, R.IshProk, R.IshPorkFIO, R.AybTopish, R.Moddasi, R.Qismi, R.Bandi, R.AyblovVozKech, R.JazoTur, R.JazoMiqdori, R.ShartliHukm, R.Modda57, R.Modda96, R.MuddatOtib, R.XavfYoqot, R.Pushaymon, R.Yarashgan, R.Kasallik, R.AktiTufay, R.userscol, R.IjYoq, R.AmaldaPushay, R.SSudHukmSana, R.SSudyaFIO, R.SAybliModda,R.SAybliQismi, R.SAybliBandi, R.SAydanVoz, R.SJazoTuri, R.SJazoMiq, R.SShartli, R.S57Modda, R.S96Modda, R.SOtibket, R.SIjXavf, R.SAmaldaPush, R.SYarshMun, R.SKasallik, R.SAktAs, R.SYoq, R.SAmal, R.Ijkattabol, R.Unchaogir, R.Ogir, R.OtaOgir, R.TumanXul, R.ShaharXul, R.BoshXul, Id], 
   (err, result) => {
     if (!err) {
       if (result.affectedRows === 0) {
@@ -153,7 +154,7 @@ app.put("/users/:id", (req, res) => {
       res.status(500).send(err)
     }
   })
-}) */
+}) 
 
 /* category */
 app.get("/category", (req, res) => {
